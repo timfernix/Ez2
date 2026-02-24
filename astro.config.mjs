1e5269203f5f680ef2234a775b3c3f1f3a0fa412
timfernix <46400905+timfernix@users.noreply.github.com>
2026-02-24 18:43:50 +0100

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://timfernix.github.io',
+  base: '/ezreal2',
+});
---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+import { z, defineCollection } from 'astro:content';
+
+const mediaCollection = defineCollection({
+  type: 'content',
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    artist: z.string(),
+    source_url: z.string().url(),
+    image: image(),
+    alt_text: z.string(),
+    skin_line: z.string(),
+    media_type: z.enum(),
+  }),
+});
+
+export const collections = {
+  'media': mediaCollection,
+};
